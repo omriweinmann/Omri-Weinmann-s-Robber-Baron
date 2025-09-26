8dc083736d7ab97b1bdc2ea02ed4b0d1010a966a
omriweinmann <omri.weinmann@gmail.com>
2025-09-25 21:43:25 -0700

    Factories

--- main.py
+++ main.py
@@ -21,6 +21,12 @@ timber_prospect = 5000
 raw_land_cost = 10000
 factory_land_cost = 20000
 
+labor1_cost = 20000
+labor2_cost = 300000
+labor3_cost = 1000000
+
+deaths = 0
+
 list_of_raw_goods = set(["Iron","Oil","Timber"])
 
 list_of_prospects = []
@@ -36,28 +42,47 @@ def fluctuate_market():
     global timber_prospect
     global raw_land_cost
     global factory_land_cost
+    global labor1_cost
+    global labor2_cost
+    global labor3_cost
     iron_prospect += max(0,int(iron_prospect * float(random.randint(-15,25))/100))
     oil_prospect += max(0,int(oil_prospect * float(random.randint(-15,25))/100))
     timber_prospect += max(0,int(timber_prospect * float(random.randint(-15,25))/100))
     raw_land_cost += max(0,int(oil_prospect * float(random.randint(-15,25))/100))
     factory_land_cost += max(0,int(timber_prospect * float(random.randint(-15,25))/100))
+    labor1_cost += max(0,int(labor1_cost * float(random.randint(-15,25))/100))
+    labor2_cost += max(0,int(labor2_cost * float(random.randint(-15,25))/100))
+    labor3_cost += max(0,int(labor3_cost * float(random.randint(-15,25))/100))
 
 def convert_to_year_quarter():
     global quarter
     quarter_s = quarter % 4
+    year = math.floor(quarter/4)
     if quarter_s == 0:
         quarter_s = 4
-    year = math.floor(quarter/4)
+        year -= 1
     return "Year "+str(year)+", Quarter "+str(quarter_s)
 
 class Prospect:
-  def __init__(self, good, id, goods_yield):
+  def __init__(self, good, plot_id, goods_yield):
     self.good = good
-    self.id = id
+    self.plot_id = plot_id
     self.goods_yield = goods_yield
 
   def __str__(self):
     return f"Your prospect for {self.good}, which has a yield of {self.goods_yield} {self.good} per quarter"
+
+list_of_factories = []
+class Factory:
+  def __init__(self, good, plot_id, goods_yield, min_deaths, max_deaths):
+    self.good = good
+    self.plot_id = plot_id
+    self.goods_yield = goods_yield
+    self.min_deaths = min_deaths
+    self.max_deaths = max_deaths
+
+  def __str__(self):
+    return f"Your factory for {self.good}, which has a yield of {self.goods_yield} {self.good} per quarter. {str(max(0,random.randint(self.min_deaths,self.max_deaths)))} workers are projected to die."
 
 prospect_id = 1
 
@@ -134,11 +159,43 @@ while True:
                     inte = int(factory_choice)
                 except:
                     print("")
-                if not inte == -1:
+                if factory_choice == "0":
+                    break
+                elif not inte == -1:
+                    continues = False
                     try:
                         print(total_list[inte-1])
-                        break
+                        continues = True
                     except:
                         print("\nInvalid plot.\n")
+                    if continues == True:
+                        plot = total_list[inte-1]
+                        if plot in list_of_prospects:
+                            print("Choose budget: \n")
+                            print(f"1: Small (96 hour work week, minimal safety standards, child labor), ${labor1_cost}")
+                            print(f"2: Medium (60 hour work week, better safety standards, no child labor), ${labor2_cost}")
+                            print(f"3: Large (40 hour work week, best safety standards, no child labor), ${labor3_cost}")
+                            print(f"\n0: Cancel")
+                            while True:
+                                labor_type_choice = input("\nWhat would you like to do? ")
+
+                                if labor_type_choice == "0":
+                                    break
+                                elif labor_type_choice == "1":
+                                    money -= labor1_cost
+                                    new_factory = Factory(plot.good,plot.plot_id,plot.goods_yield,-25,50)
+                                    list_of_factories.append(new_factory)
+                                    list_of_prospects.remove(plot)
+                                    print(f"\nYou have built a {plot.good} plant that produces {plot.goods_yield}{plot.good} per quarter.")
+                                    add_deaths = max(0,random.randint(-250,150))
+                                    print(f"\nIt cost ${labor1_cost}")
+                                    print(f"\nAdditionally it cost the lives of {add_deaths} workers.")
+                                    input("\nAre you happy with yourself?\nY/N:")
+                                    break
+                                #elif labor_type_choice == "2":
+                                    
+                                #elif labor_type_choice == "3":
+                            break
+                                    
 
 
