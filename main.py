554c8ad2ef3a6ab7e461d73d2e0e613eb14f7895
omriweinmann <omri.weinmann@gmail.com>
2025-09-27 23:06:15 -0700

    Actions n stuff

--- main.py
+++ main.py
@@ -27,11 +27,32 @@ labor1_cost = 20000
 labor2_cost = 300000
 labor3_cost = 1000000
 
+cost_to_gerrymand = 5000
+
 market = {
     "Oil": 1000,
     "Timber": 250,
     "Iron": 750,
-    "Refined Oil": 2500,
+    "Grain": 100,
+    "Refined Oil": 3500,
+    "Planks": 350,
+    "Steel": 2000,
+    "Housing": 15000,
+    "Railroad": 35000,
+    "Foodstuffs": 2000,
+    "Processed Foodstuffs": 7500,
+    "Automobiles": 10000,
+    "Ships": 100000,
+    "Paper": 2000,
+    "Newspapers": 5000,
+    "Books": 7500,
+    "Appliances": 10000,
+    "Tools": 2500,
+    "Gunpowder": 10000,
+    "Machine Guns": 20000,
+    "Bombs": 5000,
+    "Rifles": 10000,
+    "Gunpowder": 100,
 }
 
 deaths = 0
@@ -57,6 +78,7 @@ def fluctuate_market():
     global labor2_cost
     global labor3_cost
     global market
+    global cost_to_gerrymand
     iron_prospect += max(0,int(iron_prospect * float(random.randint(-15,25))/100))
     oil_prospect += max(0,int(oil_prospect * float(random.randint(-15,25))/100))
     timber_prospect += max(0,int(timber_prospect * float(random.randint(-15,25))/100))
@@ -65,6 +87,7 @@ def fluctuate_market():
     labor1_cost += max(0,int(labor1_cost * float(random.randint(-15,25))/100))
     labor2_cost += max(0,int(labor2_cost * float(random.randint(-15,25))/100))
     labor3_cost += max(0,int(labor3_cost * float(random.randint(-15,25))/100))
+    cost_to_gerrymand += max(0,int(cost_to_gerrymand * float(random.randint(-15,35))/100))
     for m in market:
         market[m] += max(0,int(market[m] * float(random.randint(-5,10))/100))
 
@@ -178,7 +201,24 @@ street_endings = [
 ]
 
 factory_types = {
-    "Refined Oil": {"Oil": 3}
+    "Refined Oil": {"Oil": 3},
+    "Planks": {"Timber": 1},
+    "Steel": {"Iron": 2},
+    "Housing": {"Steel": 3, "Planks": 5},
+    "Railroad": {"Steel": 10},
+    "Foodstuffs": {"Grain": 5},
+    "Processed Foodstuffs": {"Foodstuffs": 5},
+    "Automobiles": {"Steel": 2, "Refined Oil": 1},
+    "Ships": {"Steel": 20, "Refined Oil": 5},
+    "Paper": {"Timber": 1},
+    "Newspapers": {"Paper": 1},
+    "Books": {"Paper": 1},
+    "Appliances": {"Paper": 1},
+    "Tools": {"Planks": 1},
+    "Gunpowder": {"Paper": 1},
+    "Machine Guns": {"Gunpowder": 10, "Steel": 10},
+    "Bombs": {"Gunpowder": 10,"Steel": 2},
+    "Rifles": {"Gunpowder": 5,"Steel": 10},
 }
 
 class Property:
@@ -202,11 +242,12 @@ class Factory:
     self.needs = needs
 
   def __str__(self):
-    string = f"Your factory for {self.good}, which has a yield of {self.goods_yield} {self.good} per quarter. {str(max(0,random.randint(self.min_deaths,self.max_deaths)))} workers are projected to die."
     if self.needs == {}:
+        string = f"Your factory for {self.good}, which has a yield of {self.goods_yield} {self.good} per quarter. {str(max(0,random.randint(self.min_deaths,self.max_deaths)))} workers are projected to die."
         return string
     else:
-        extrastring = "\n - To produce this, the factory requires an input of "
+        string = f"Your factory for {self.good}, which has a yield of {self.goods_yield*3} {self.good} when its input needs are fully met. {str(max(0,random.randint(self.min_deaths,self.max_deaths)))} workers are projected to die."
+        extrastring = "\n - To produce this, the factory requires a minimum input of "
         x = 0
         for need in self.needs:
             x += 1
@@ -218,12 +259,24 @@ class Factory:
 
 prospect_id = 1
 
+political_standing = 10
+
+conservatives = 0
+liberals = 0
+reactionaries = 0
+
+bailout = False
+
 goods = {}
 
 while True:
     if money < 0:
         print(f"\n\n\nYou are in insurmountable amounts of debt! Game over.\n\nYour actions caused the deaths of {deaths} people.\n\nGame Over! You ended with ${money}\n\n\n")
         break
+    political_standing = max(10,political_standing-5)
+    conservatives = max(0,conservatives-5)
+    liberals = max(0,liberals-5)
+    reactionaries = max(0,reactionaries-5)
     for factory_raw in list_of_factories:
         factory:Factory = factory_raw
         go = True
@@ -260,13 +313,21 @@ while True:
     quarter += 1
     print("\n"+convert_to_year_quarter())
     if not quarter == 1 and quarter % 4 == 1:
-        tax = int((max(money,0) * 0.07) + 20000)
+        tax = int((max(money,0) * 0.07) + (15000 * ((400-(political_standing+conservatives+liberals+reactionaries))/800+0.5)))
         print(f"\nYou have lost ${tax} due to taxes and other miscellaneous costs.")
         money -= tax
     print("\nGood morning, '"+str(first_name)+".' Your company, '"+str(company_name)+"', currently has $"+str(money)+".\n")
 
     if quarter == 1:
         print("I suggest you prospect for resources; you cant make a factory if you dont know where to place one!")
+    if quarter == 2:
+        print("If you have not already, I suggest you build a factory on your prospect to start making goods")
+    if quarter == 3:
+        print("Export your goods to start making money!")
+    if quarter == 4:
+        print("At the start of every year you are taxed.")
+    if quarter == 5:
+        print("Buy properties to build new types of factories and eventually make elaborate industry chains!")
     xxx = 0
     while True:
         if not xxx == 0:
@@ -278,7 +339,8 @@ while True:
         print("2: Buy a property")
         print("3: Build a factory")
         print("4: Import/Export")
-        print("5: Company information")
+        print("5: Actions")
+        print("6: Company information")
 
         quarterly_choice = input("\n")
 
@@ -413,7 +475,7 @@ while True:
                                     string = string + f"{factory_types[f][x]} {x},"
                                 print(f"{f}:"+string)
                             while True:
-                                property_choice = input("\nWhat would you like to do?")
+                                property_choice = input("\nWhat would you like to do? ")
                                 if property_choice == 0:
                                     break
                                 if property_choice in factory_types:
@@ -429,7 +491,7 @@ while True:
                                             break
                                         elif labor_type_choice == "1":
                                             money -= labor1_cost
-                                            new_factory = Factory(property_choice,0,1,-25,25,factory_types[f])
+                                            new_factory = Factory(property_choice,0,1,-25,25,factory_types[property_choice])
                                             list_of_factories.append(new_factory)
                                             list_of_properties.remove(plot)
                                             print(f"\nYou have built a {new_factory.good} plant that produces {new_factory.goods_yield} {new_factory.good} per quarter.")
@@ -442,7 +504,7 @@ while True:
                                             break
                                         elif labor_type_choice == "2":
                                             money -= labor2_cost
-                                            new_factory = Factory(property_choice,0,1,-35,10,factory_types[f])
+                                            new_factory = Factory(property_choice,0,1,-35,10,factory_types[property_choice])
                                             list_of_factories.append(new_factory)
                                             list_of_properties.remove(plot)
                                             print(f"\nYou have built a {new_factory.good} plant that produces {new_factory.goods_yield} {new_factory.good} per quarter.")
@@ -455,7 +517,7 @@ while True:
                                             break
                                         elif labor_type_choice == "3":
                                             money -= labor3_cost
-                                            new_factory = Factory(property_choice,0,1,-50,2,factory_types[f])
+                                            new_factory = Factory(property_choice,0,1,-50,2,factory_types[property_choice])
                                             list_of_factories.append(new_factory)
                                             list_of_properties.remove(plot)
                                             print(f"\nYou have built a {new_factory.good} plant that produces {new_factory.goods_yield} {new_factory.good} per quarter.")
@@ -467,9 +529,9 @@ while True:
                                             input("\nAre you happy with yourself?\nY/N:")
                                             break
                                     break
-        elif quarterly_choice == "5":
+        elif quarterly_choice == "6":
             while True:
-                info_choice = input("\nWhat would you like to look at?\n\n0: Go back\n\n1: List of factories\n2: List of goods\n3: Market \n\n")
+                info_choice = input("\nWhat would you like to look at?\n\n0: Go back\n\n1: List of factories\n2: List of goods\n3: Market \n4: Politics\n\n")
 
                 if info_choice == "0":
                     break
@@ -491,6 +553,15 @@ while True:
                     x = 0
                     for good in market:
                         print(str(good)+": $"+str(market[good]))
+                elif info_choice == "4":
+                    print(f"\nYour political standing is: {political_standing}/100")
+                    print(f"\nYour conservative support is: {conservatives}/100")
+                    print(f"Your liberal support is: {liberals}/100")
+                    print(f"Your reactionary support is: {reactionaries}/100")
+                    if bailout == True:
+                        print("\nYou have used your government bailout.\n")
+                    else:
+                        print("\nYou have not used your government bailout.\n")
         elif quarterly_choice == "2":
             while True:
                 potential_property = Property()
@@ -548,6 +619,111 @@ while True:
                         print("\nInvalid amount\n")
                 else:
                     print("\nInvalid good\n")
+        elif quarterly_choice == "5":
+            while True:
+                print(f"\n0: Cancel\n")
+                print("1: Political actions")
+                print("2: Bailout (If you are in debt, this will save you depending on your political standings, but this will prevent you from making political actions)")
+                print(f"3: Lobby for war (${cost_to_gerrymand*6}) (war related goods will increase in value)")
+                print(f"4: Push for reforms (${cost_to_gerrymand*4}) (Political standing and will increase)")
+                print(f"5: Roll back reforms (Gain ${cost_to_gerrymand*10} from reactionaries) (Lots of support needed)")
+                action_choice = input("\nWhat would you like to do? ")
+                if action_choice == "0":
+                    break 
+                elif action_choice == "1":
+                    if bailout == False:
+                        while True:
+                            print(f"\n0: Go back\n")
+                            print("1: Gerrymander (Increases political standing by 20)")
+                            print("2: Donate (Increases political standing by 10)")
+                            print("3: Increase conservative support (Requires a political standing of 40)")
+                            print("4: Increase liberal support (Requires a political standing of 40)")
+                            print("5: Increase reactionary support (Requires a political standing of 40)")
+                            political_action_choice = input("\nWhat would you like to do? ")
+                            if political_action_choice == "0":
+                                break
+                            elif political_action_choice == "1":
+                                p_action_confirm_choice = input(f"\nGerrymandering will cost you ${int(cost_to_gerrymand*2)}, are you sure? Y/N: ")
+                                if p_action_confirm_choice == "Y":
+                                    political_standing = min(100,political_standing+20)
+                                    money -= int(cost_to_gerrymand*2)
+                                    print(f"\nYour political standing is now {political_standing}/100")
+                            elif political_action_choice == "2":
+                                p_action_confirm_choice = input(f"\Donating will cost you ${int(cost_to_gerrymand)}, are you sure? Y/N: ")
+                                if p_action_confirm_choice == "Y":
+                                    political_standing = min(100,political_standing+10)
+                                    money -= int(cost_to_gerrymand)
+                                    print(f"\nYour political standing is now {political_standing}/100")
+                            elif political_action_choice == "3":
+                                if political_standing >= 40:
+                                    p_action_confirm_choice = input(f"\Increasing conservative support will cost you ${int(cost_to_gerrymand)}, are you sure? Y/N: ")
+                                    if p_action_confirm_choice == "Y":
+                                        political_standing = min(100,conservatives+20)
+                                        money -= int(cost_to_gerrymand)
+                                        print(f"\nYour standing with the conservatives is now {conservatives}/100")
+                                else:
+                                    print("\nNot enough political standing\n")
+                            elif political_action_choice == "4":
+                                if political_standing >= 40:
+                                    p_action_confirm_choice = input(f"\Increasing liberal support will cost you ${int(cost_to_gerrymand)}, are you sure? Y/N: ")
+                                    if p_action_confirm_choice == "Y":
+                                        political_standing = min(100,liberals+20)
+                                        money -= int(cost_to_gerrymand)
+                                        print(f"\nYour standing with the liberals is now {liberals}/100")
+                                else:
+                                    print("\nNot enough political standing\n")
+                            elif political_action_choice == "5":
+                                if political_standing >= 40:
+                                    p_action_confirm_choice = input(f"\Increasing reactionary support will cost you ${int(cost_to_gerrymand)}, are you sure? Y/N: ")
+                                    if p_action_confirm_choice == "Y":
+                                        political_standing = min(100,reactionaries+20)
+                                        money -= int(cost_to_gerrymand)
+                                        print(f"\nYour standing with the reactionaries is now {reactionaries}/100")
+                                else:
+                                    print("\nNot enough political standing\n")
+                    else:
+                        print("\nYou have used your bailout, and cannot make anymore political actions.")
+                elif action_choice == "2":
+                    if money < 0:
+                        political_bailout = political_standing*150
+                        conservative_bailout = conservatives*100
+                        liberal_bailout = liberals*100
+                        reactionary_bailout = reactionaries*100
+                        print(f"The government has given you ${political_bailout}")
+                        print(f"\nIn addition, conservatives have given you ${conservative_bailout}")
+                        print(f"In addition, liberals have given you ${liberal_bailout}")
+                        print(f"In addition, reactionaries have given you ${reactionary_bailout}")
+                    else:
+                        print("\nYou are not in debt.")
+                elif action_choice == "3":
+                    if reactionaries + conservatives > (75-liberals):
+                        print("Your country is going to war because of you, war related goods have increased in value")
+                        add_deaths = random.randint(100,900000)
+                        deaths += add_deaths
+                        money -= cost_to_gerrymand*6
+                        print(f"\nThe war resulted in {add_deaths} casualties")
+                        market["Gunpowder"] += int(market["Gunpowder"] * (random.randint(0,50)/100))
+                        market["Rifles"] += int(market["Rifles"] * (random.randint(0,50)/100))
+                        market["Machine Guns"] += int(market["Machine Guns"] * (random.randint(0,50)/100))
+                        market["Bombs"] += int(market["Bombs"] * (random.randint(0,50)/100))
+                    else:
+                        print("You need to have more support (especially from conservatists and reactionaries)")
+                elif action_choice == "4":
+                    if liberals > (150-(conservatives+reactionaries)):
+                        print("You have succeeded in pushing for reforms")
+                        money -= cost_to_gerrymand*4
+                        political_standing = min(100,political_standing+20)
+                        print(f"\nYour political standing is now {political_standing}/100")
+                    else:
+                        print("You need to have more support (especially from liberals)")
+                elif action_choice == "5":
+                    if reactionaries > (275-(conservatives+liberals+political_standing)):
+                        print("You have succeeded in rolling back reforms")
+                        money += cost_to_gerrymand*10
+                        print(f"\nReactionaries have given you ${cost_to_gerrymand*8}")
+                    else:
+                        print("You need to have more support (especially from reactionaries)")
+
 
 
 
